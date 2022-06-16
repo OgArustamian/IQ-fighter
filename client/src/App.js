11e70b2dddcd5d3ea1a4a6820ec3901e7e5614ac
EugeneS <audoken2125@gmail.com>
2022-06-16 12:28:06 +0300

    Main navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import MainNavbar from './Components/MainNavbar/MainNavbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <MainNavbar />
     </div>
   );
 }
