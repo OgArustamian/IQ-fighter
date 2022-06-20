a2c06b49f10b843847505d431284d8359d3b6e27
EugeneS <audoken2125@gmail.com>
2022-06-20 12:47:09 +0300

    ..

--- client/src/Redux/Reducers/rootReducer.js
+++ client/src/Redux/Reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import answersReducer from './answersReducer';
 import usersReducer from './usersReducer';
 
 const rootReducer = combineReducers({
   users: usersReducer,
+  answers: answersReducer,
 });
 
 export default rootReducer;
--- client/src/Redux/Types/types.js
+++ client/src/Redux/Types/types.js
@@ -1,2 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const SET_USER = 'SET_USER';
+export const SET_ANSWER = 'SET_ANSWER';
