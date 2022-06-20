78057ab2153ed7f759bf594a24f13125a8168bed
Victor Karvatsky <victor.karvatsky@gmail.com>
2022-06-20 20:09:30 +0300

    game controller 1

--- server/ws/roomController.js
+++ server/ws/roomController.js
@@ -4,7 +4,7 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-restricted-syntax */
 
-const { Game } = require('../db/models');
+const { Game, Turn } = require('../db/models');
 
 // function generalInformation(ws) {
 //   let obj;
@@ -44,10 +44,11 @@ async function create(ws, userID, rooms) {
   const room = genKey(5);
   console.log('create', room); // information
   rooms[room] = [ws];
-  // const game = await Game.create({ winner_id: 0 });
+  const game = await Game.create({ winner_id: 1 });
+  const turn = await Turn.create({ game_id: game.id });
   ws.room = room;
-  // ws.game = game.id;
-  ws.send(JSON.stringify({ type: 'createdRoom', params: { room } }));
+  ws.gameID = game.id;
+  ws.send(JSON.stringify({ type: 'createdRoom', params: { room, gameID: game.id, turnID: turn.id } }));
 
   // generalInformation(ws);
 }
@@ -66,13 +67,13 @@ function join(rooms, maxClients, ws, userID, room) {
   rooms[room].push(ws);
   ws.userID = userID;
   ws.room = room;
-  // const { game } = rooms[room][0];
-  // ws.game = game;
-  // console.log('game in ws', ws.game);
+  const { gameID } = rooms[room][0];
+  ws.gameID = gameID;
+  console.log('game in ws', ws.gameID);
 
   console.log('join', room); // information
 
-  rooms[room].forEach((el) => el.send(JSON.stringify({ type: 'joinedRoom', params: { room } })));
+  rooms[room].forEach((el) => el.send(JSON.stringify({ type: 'joinedRoom', params: { room, gameID } })));
   console.log('send join true');
 
   // generalInformation(ws);
@@ -89,7 +90,7 @@ function leave(rooms, ws) {
   try {
     rooms[room] = rooms[room].filter((so) => so !== ws);
     ws.room = undefined;
-    // ws.game = undefined;
+    ws.game = undefined;
   } catch (err) { console.error(err); }
 
   if (rooms[room].length == 0) { close(rooms, room); }
