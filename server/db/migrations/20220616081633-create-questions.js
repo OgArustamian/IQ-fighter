6e0f6e3b8370059e9a3721fcc39528fbef475b68
Evgeniy <savinev3@live.ru>
2022-06-20 09:32:21 +0300

    Created questions about JS

--- server/db/migrations/20220616081633-create-questions.js
+++ server/db/migrations/20220616081633-create-questions.js
@@ -8,7 +8,7 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
       question: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull: false,
       },
       url: {
