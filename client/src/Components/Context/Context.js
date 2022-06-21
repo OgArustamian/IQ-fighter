3b56660260551748c4f492f3d2662ddbc1bf6dec
EugeneS <audoken2125@gmail.com>
2022-06-21 16:28:40 +0300

    healthbar logic

--- client/src/Components/Context/Context.js
+++ client/src/Components/Context/Context.js
@@ -17,14 +17,17 @@ function Context({ children }) {
   const [modal, setModal] = useState(false);
   const dispatch = useDispatch();
   const { id } = useSelector((state) => state.users);
-  const [playerHp, setPlayerHp] = useState(null);
+  const [firstPlayerHp, setFirstPlayerHp] = useState(null);
+  const [secondPlayerHp, setSecondPlayerHp] = useState(null);
+  const { player } = useSelector((state) => state);
+  const [hero, setHero] = useState(null);
+  const [enemy, setEnemy] = useState(null);
 
   ws.onmessage = (event) => {
     const { type, params } = JSON.parse(event.data);
     const {
       room, gameID, turnID, hp,
     } = params;
-    console.log(hp);
 
     switch (type) {
       case ATTACK:
@@ -41,7 +44,8 @@ function Context({ children }) {
         dispatch(setRoom(room));
         dispatch(setGame(gameID, turnID));
         dispatch(showSpinner(type));
-        setPlayerHp(hp);
+        setFirstPlayerHp(hp);
+        setSecondPlayerHp(hp);
         break;
 
       case 'draw':
@@ -51,12 +55,13 @@ function Context({ children }) {
 
       case 'win':
         console.log('WIN------------------>', JSON.parse(event.data));
+        setSecondPlayerHp(hp);
         dispatch(changeTurn());
         break;
 
       case 'loss':
         console.log('LOSS------------------>', JSON.parse(event.data));
-        setPlayerHp(hp);
+        firstPlayerHp(hp);
         dispatch(changeTurn());
         break;
 
@@ -67,11 +72,18 @@ function Context({ children }) {
   };
   useEffect(() => {
     setWs(new WebSocket('ws://localhost:3001'));
+    if (player.position === 'left') {
+      setHero(true);
+      setEnemy(false);
+    } else {
+      setEnemy(true);
+      setHero(false);
+    }
   }, [id]);
 
   return (
     <WsContext.Provider value={{
-      ws, modal, setModal, playerHp,
+      ws, modal, setModal, hero, enemy, firstPlayerHp, secondPlayerHp,
     }}
     >
       {children}
